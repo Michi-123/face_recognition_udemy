7f362a1aaaef924a964452f03c42ca53c878ceaa
Michiyasu Uchiyama <uchi.udemy@gmail.com>
2025-02-14 12:21:34 +0900

    Update setup.py
    v 2.0.0

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'face_recognition_models>=0.3.0',
+    'face_recognition_models==0.3.0',
     'Click>=6.0',
-    'dlib>=19.7',
+    'dlib==19.18.',
     'numpy',
     'Pillow'
 ]
@@ -23,13 +23,52 @@ test_requirements = [
 ]
 
 setup(
-    name='face_recognition',
-    version='1.4.0',
+    name='face_recognition_udemy',
+    version='2.0.0',
     description="Recognize faces from Python or from the command line",
     long_description=readme + '\n\n' + history,
     author="Adam Geitgey",
-    author_email='ageitgey@gmail.com',
-    url='https://github.com/ageitgey/face_recognition',
+    
+    url='https://github.com/Michi123/face_recognition_udemy',
+    packages=[
+        'face_recognition',
+    ],
+    package_dir={'face_recognition': 'face_recognition'},
+    package_data={
+        'face_recognition': ['models/*.dat']
+    },
+    entry_point#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'face_recognition_models==0.3.0',
+    'Click>=6.0',
+    'dlib==19.18.0',
+    'numpy',
+    'Pillow'
+]
+
+test_requirements = [
+    'tox',
+    'flake8'
+]
+
+setup(
+    name='face_recognition_udemy',
+    version='2.0.0',
+    description="Recognize faces from Python or from the command line",
+    long_description=readme + '\n\n' + history,
+    author="Adam Geitgey",
+    
+    url='https://github.com/Michi123/face_recognition_udemy',
     packages=[
         'face_recognition',
     ],
@@ -46,7 +85,32 @@ setup(
     install_requires=requirements,
     license="MIT license",
     zip_safe=False,
-    keywords='face_recognition',
+    keywords='face_recognition_udemy',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
+s={
+        'console_scripts': [
+            'face_recognition=face_recognition.face_recognition_cli:main',
+            'face_detection=face_recognition.face_detection_cli:main'
+        ]
+    },
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='face_recognition_udemy',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
