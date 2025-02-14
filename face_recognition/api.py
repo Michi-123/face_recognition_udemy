1a4653b6ca8f5d51a326ae35128d461efa0baad8
Michiyasu Uchiyama <uchi.udemy@gmail.com>
2025-02-14 11:28:46 +0900

    Update api.py
    Disabled CUDA process at cnn_face_detector.

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -22,8 +22,8 @@ pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model)
 predictor_5_point_model = face_recognition_models.pose_predictor_five_point_model_location()
 pose_predictor_5_point = dlib.shape_predictor(predictor_5_point_model)
 
-cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
-cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
+cnn_face_detection_model = None#face_recognition_models.cnn_face_detector_model_location()
+cnn_face_detector = None#dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
 
 face_recognition_model = face_recognition_models.face_recognition_model_location()
 face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
